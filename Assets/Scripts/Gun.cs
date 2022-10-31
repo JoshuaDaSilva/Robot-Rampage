87be344cd430456d80306fa340d12b6edbe1ed85
Joshua Da Silva <joshua.dasilva@student.sl.on.ca>
2022-10-31 13:49:46 -0400

    Chapter 10 - Creating the Robots

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -21,8 +21,7 @@ public class Gun : MonoBehaviour
     {
     }
     protected void Fire()
-    {
-      
+    { 
         if (ammo.HasAmmo(tag))
         {   
             GetComponent<AudioSource>().PlayOneShot(liveFire);
