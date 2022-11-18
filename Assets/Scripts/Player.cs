1d06f109e908d11c2ec7bcdf25532a47565c2d79
Joshua Da Silva <joshua.dasilva@student.sl.on.ca>
2022-11-17 19:46:35 -0500

    Chapter 11 - Adding UI Elements

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,42 +10,29 @@ public class Player : MonoBehaviour
     private GunEquipper gunEquipper;
     private Ammo ammo;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        ammo = GetComponent<Ammo>();
-        gunEquipper = GetComponent<GunEquipper>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public void TakeDamage(int amount)
     {
-     int healthDamage = amount;
-     if (armor > 0) 
-     {
-     int effectiveArmor = armor * 2;
-        effectiveArmor -= healthDamage;
-     // If there is still armor, don't need to process
-     // health damage
-     if (effectiveArmor > 0) 
-     {
-     armor = effectiveArmor / 2;
-     return;
-     }
+        int healthDamage = amount;
+        if (armor > 0)
+        {
+            int effectiveArmor = armor * 2;
+            effectiveArmor -= healthDamage;
+            // If there is still armor, don't need to process
+            // health damage
+            if (effectiveArmor > 0)
+            {
+                armor = effectiveArmor / 2;
+                return;
+            }
 
-     armor = 0;
-     }
-     health -= healthDamage;
-    Debug.Log("Health is " + health);
-    if (health <= 0)
-    {
-        Debug.Log("GameOver");
-    }
+            armor = 0;
+        }
+        health -= healthDamage;
+        Debug.Log("Health is " + health);
+        if (health <= 0)
+        {
+            Debug.Log("GameOver");
+        }
     }
 
     // 1
@@ -98,5 +85,18 @@ public class Player : MonoBehaviour
                 Debug.LogError("Bad pickup type passed" + pickupType);
                 break;
         }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ammo = GetComponent<Ammo>();
+        gunEquipper = GetComponent<GunEquipper>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
     }
 }
